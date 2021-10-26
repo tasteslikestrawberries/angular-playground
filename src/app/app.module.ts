441b2ff866e08f160875797094abbc87f06ee22b
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-10-26 21:45:45 +0200

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 //components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navigation/navbar/navbar.component';
 import { PlaygroundComponent } from './components/playground/playground.component';
 import { AboutComponent } from './components/about/about.component';
+
+//styles and icons modules
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,7 @@ import { AboutComponent } from './components/about/about.component';
     NavbarComponent,
     AboutComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/navigation/navbar/navbar.component.ts
+++ src/app/components/navigation/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  show:boolean = false;
+  faBars = faBars;
+  faTimes = faTimes;
+
+  toggle = () => {
+    this.show = !this.show; 
+  }
 
   constructor() { }
 
