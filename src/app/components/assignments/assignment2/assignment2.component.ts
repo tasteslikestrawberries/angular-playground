08bb077586daafff930b328c5dbe7912cdc67760
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-10-29 13:02:23 +0200

    finished assignment 2

--- src/app/components/assignments/assignment2/assignment2.component.ts
+++ src/app/components/assignments/assignment2/assignment2.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment2.component.css']
 })
 export class Assignment2Component implements OnInit {
+  show: boolean = false;
+  logArr: number[] = [];
+
+  logger = () => {
+    this.show = !this.show;
+    this.logArr.push(this.logArr.length + 1)
+  }
 
   constructor() { }
 
