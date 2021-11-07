64f8c4542344cff15b2904e48f108ecdec1e347c
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-11-07 20:38:10 +0100

    up

--- src/app/components/playground/playground.component.ts
+++ src/app/components/playground/playground.component.ts
@@ -1,25 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
-  styleUrls: ['./playground.component.css']
+  styleUrls: ['./playground.component.css'],
 })
 export class PlaygroundComponent implements OnInit {
   myArr = [
-    {type:'fruit', name:'apple', price:5 },
-    {type:'vegetable', name:'tomato', price:4 },
-    {type:'dairy', name:'milk', price:6 }
-  ]
-  
-  addElementsToArray = () => {
-    this.myArr.push( {type:'fruit', name:'apple', price:10 });
-    console.log(this.myArr)
-  }
+    { type: 'fruit', name: 'apple', price: 5 },
+    { type: 'vegetable', name: 'tomato', price: 4 },
+    { type: 'dairy', name: 'milk', price: 6 },
+  ];
 
-  constructor() { }
+  //data: any = [];
+
+  constructor(private http: HttpClient) {}
+
+  addElementsToArray = () => {
+    this.myArr.push({ type: 'fruit', name: 'apple', price: 10 });
+    console.log(this.myArr);
+  };
+
+  /*postData() {
+    this.http.post('someurl.com/posts.json', postData).subscribe((data) => {
+      console.log(data);
+    });
+  }*/
+
+  /*getData() {
+    this.http.get('https://randomuser.me/api').subscribe((data) => {
+      console.log(data);
+    });
+  }*/
 
   ngOnInit(): void {
   }
-
 }
