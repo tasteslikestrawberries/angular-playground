06759438612959b2d9019842ef746feeadf924cd
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-11-08 20:47:22 +0100

    finished template driven forms

--- src/app/components/playground/forms-learning/forms-learning.component.ts
+++ src/app/components/playground/forms-learning/forms-learning.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-learning',
   templateUrl: './forms-learning.component.html',
-  styleUrls: ['./forms-learning.component.css']
+  styleUrls: ['./forms-learning.component.css'],
 })
 export class FormsLearningComponent implements OnInit {
+  @ViewChild('f') signupForm?: NgForm;
 
-  constructor() { }
+  username = 'Default Username';
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['Female', 'Male'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.signupForm);
+    this.user.username = this.signupForm?.value.username;
+    this.user.email = this.signupForm?.value.email;
+    this.user.secretQuestion = this.signupForm?.value.secretQuestion;
+    this.user.answer = this.signupForm?.value.answer;
+    this.user.gender = this.signupForm?.value.gender;
+
+    this.signupForm?.reset(); //resets the form and its properties and state (like touched,dirty etc.)
   }
 
+  ngOnInit(): void {}
 }
