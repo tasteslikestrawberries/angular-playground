49c77825f326cafb7716498a5ac907ce6d6c67ed
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-11-09 13:39:51 +0100

    finished reactive forms

--- src/app/components/lessons/lesson6/reactive-approach/reactive-approach.component.ts
+++ src/app/components/lessons/lesson6/reactive-approach/reactive-approach.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-approach',
@@ -14,14 +14,35 @@ export class ReactiveApproachComponent implements OnInit {
 
   //form should be initialized here, before the template is rendered
   ngOnInit(): void {
-    //we pass 2 arguments
-    //the first one is the initial state, the initial value of the form
-    //the second is a single validator or an array of validators
-    //the third will be potential asynchronous validators
     this.signupForm = new FormGroup({
-      username: new FormControl(null),
-      email: new FormControl(null),
+      //here we get the access to the form so we don't have to use @ViewChild
+      //here we declare our form controls that show up as values when console logging our form element (this.signupForm)
+      //we pass 2 arguments in every FormControl ( , )
+      //the first one is the initial state, the initial value of the form
+      //the second is a single validator or an array of validators
+      //the third will be potential asynchronous validators
+      //we don't call the Validators.required method, we are just passing a reference to it
+      //Angular will execute this Validators.required method whenever it detects that input of that FormControl changed
+      userData: new FormGroup({
+        //nesting form-controls in the userData formgroup
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
       gender: new FormControl('male'),
+      hobbies: new FormArray([]), //array of form controls
     });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
   }
 }
