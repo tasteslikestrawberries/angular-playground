ad95572759c4f09872c4c60d6163c0b494ce3da3
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-11-10 10:05:07 +0100

    finished login method with error handling

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthService, IAuthResponseData } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -24,22 +25,31 @@ export class AuthComponent implements OnInit {
     }
     const email = form.value.email;
     const password = form.value.password;
+
+    let authObs: Observable<IAuthResponseData>;
+
     this.isLoading = true;
+    //after this if statement, certainly at least one of this two observables will be stored in authObs
+    /* we are actually reducing the code here, so we don't have to repeat the exact same code for signup and login subscribing, but rather
+    doing it once below (authObs.subscribe)*/
+
     if (this.isLoginMode) {
-      //...
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(
-        (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        (error) => {
-          console.log(error);
-          this.error= 'An error occured!';
-          this.isLoading = false;
-        }
-      );
+      authObs = this.authService.signup(email, password);
     }
+
+    authObs.subscribe(
+      (resData) => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      (errorMsg) => {
+        console.log(errorMsg);
+        this.error = errorMsg;
+        this.isLoading = false;
+      }
+    );
 
     form.reset();
   }
--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -1,13 +1,16 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-//https://firebase.google.com/docs/reference/rest/auth
-interface IAuthResponseData {
-  idToken:string;
-  email:string;
-  refreshToken: string;
-  expiresIn:string;
-  localId:string;
+//https://firebase.google.com/docs/reference/rest/auth - response payload
+export interface IAuthResponseData {
+  idToken: string; //A Firebase Auth ID token for the authenticated user.
+  email: string; //	The email for the authenticated user.
+  refreshToken: string; //	A Firebase Auth refresh token for the authenticated user.
+  expiresIn: string; //	The number of seconds in which the ID token expires.
+  localId: string; //The uid of the authenticated user.
+  registered?: boolean; //Whether the email is for an existing account, signup doesn't yield it but login does
 }
 
 @Injectable({
@@ -17,13 +20,51 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
-    return this.http.post<IAuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDWfBOFdgr1Lx0cKLXG6l8GGDt8NP2v9O8', //firebase apikey
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true
-      }
-    );
+    return this.http
+      .post<IAuthResponseData>(
+        //firebase signup api endpoint + apikey
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDWfBOFdgr1Lx0cKLXG6l8GGDt8NP2v9O8',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<IAuthResponseData>(
+        //firebase sign-in/login api endpoint + apikey
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDWfBOFdgr1Lx0cKLXG6l8GGDt8NP2v9O8',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  //this method is private because it will be used only inside this service
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMsg = 'An unknown error occured!'; //this will be the default error message
+    //if error response doesn't have an error key or doesn't have an error key on the error key (nested error key)
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMsg);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS': //this is firebase's api common error code(https://firebase.google.com/docs/reference/rest/auth)
+        errorMsg = 'This email exists already.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMsg = 'This email does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMsg = 'This password is not correct.';
+        break;
+    }
+    return throwError(errorMsg);
   }
 }
