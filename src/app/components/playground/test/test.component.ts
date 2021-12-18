4a074b154ba5b7f5d3ad3ee9b328e7070aff66b1
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-12-18 20:03:19 +0100

    up

--- src/app/components/playground/test/test.component.ts
+++ src/app/components/playground/test/test.component.ts
@@ -15,6 +15,8 @@ export class TestComponent implements OnInit, OnDestroy {
 
   @ViewChild('lazy')
   lazy?: ElementRef<HTMLDivElement>;
+  @ViewChild('lazy2')
+  lazy2?: ElementRef<HTMLDivElement>;
 
 
   constructor() { }
@@ -32,35 +34,51 @@ export class TestComponent implements OnInit, OnDestroy {
 
   //intersection observer
   ngAfterViewInit() {
-    const ioCallback: IntersectionObserverCallback = (entries, observer) => {
-      if (entries[0].isIntersecting) {
-        observer.unobserve(this.lazy!.nativeElement);
-
-        const lazyElements =
-          this.lazy!.nativeElement.querySelectorAll('[data-lazy]');
-
-        lazyElements.forEach((el: any) => {
-          el.classList.add(el.dataset.lazy);
-          delete el.dataset.lazy;
-        });
-        console.log(lazyElements);
-      }
-    };
-    const ioOptions: IntersectionObserverInit = {
-      threshold: 0.3,
-      rootMargin: '0px 0px -20px 0px',
-    }; //A threshold of 1.0 means that when 100% of the target is visible within the element specified by the root option, the callback is invoked.
-    const intersectionObserver = new IntersectionObserver(
-      ioCallback,
-      ioOptions
-    ); //Creating the intersection observer by calling its constructor and passing it a callback function to be run whenever a threshold is crossed in one direction or the other
-    intersectionObserver.observe(this.lazy!.nativeElement);
+    createLazyElement([this.lazy!.nativeElement, this.lazy2!.nativeElement], {})
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
+}
 
+/**
+ * 
+ * @param elements 
+ * @param intersectionObserverInit 
+ * * root - viewport by default. 
+ * * threshold - how much of the element is the viewport. 
+ * * rootMargin 
+ */
+function createLazyElement(elements: Element[], intersectionObserverInit?: IntersectionObserverInit) {
+  const ioCallback: IntersectionObserverCallback = (entries, observer) => {
+    for (const entry of entries) {
+      console.log(entry)
+      if (!entry.isIntersecting) {
+        continue; //  if entry(element) is not intersecting (not visible) then go to the next entry
+      }
+
+      observer.unobserve(entry.target);
+
+      const lazyElements =
+      entry.target.querySelectorAll('[data-lazy]');
+
+      lazyElements.forEach((el: any) => {
+        el.classList.add(el.dataset.lazy);
+        delete el.dataset.lazy;
+      });
+      //console.log(lazyElements);
+    }
+  };
+  let ioOptions: IntersectionObserverInit = {
+    threshold: 0.3,
+  }; //A threshold of 1.0 means that when 100% of the target is visible within the element specified by the root option, the callback is invoked.
+  ioOptions = {...ioOptions, ...intersectionObserverInit}
+  const intersectionObserver = new IntersectionObserver(
+    ioCallback,
+    ioOptions
+  ); //Creating the intersection observer by calling its constructor and passing it a callback function to be run whenever a threshold is crossed in one direction or the other
+  elements.forEach((element) => intersectionObserver.observe(element))
 
 }
