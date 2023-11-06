6bc0a43040e2a10fe0909d9eee9ab69b4789ab75
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2023-11-06 08:44:52 +0100

    configures jest

--- src/app/components/signals/signals.component.spec.ts
+++ src/app/components/signals/signals.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignalsComponent } from './signals.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('SignalsComponent', () => {
   let component: SignalsComponent;
@@ -8,7 +9,8 @@ describe('SignalsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SignalsComponent]
+      declarations: [SignalsComponent],
+      providers: [HttpClientModule]
     });
     fixture = TestBed.createComponent(SignalsComponent);
     component = fixture.componentInstance;
--- src/app/directives/dynamic-loader.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DynamicLoaderDirective } from './dynamic-loader.directive';
-
-describe('DynamicLoaderDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DynamicLoaderDirective();
-    expect(directive).toBeTruthy();
-  });
-});
