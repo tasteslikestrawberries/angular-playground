4198d011d277a9b9e0c67de141e7a43e2dc56d93
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2023-11-16 19:24:24 +0100

    test

--- src/app/components/lazy-loading/lazy-loading.component.ts
+++ src/app/components/lazy-loading/lazy-loading.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, signal } from '@angular/core';
-import { LoadingSkeletonComponent } from './loading-skeleton/loading-skeleton.component';
+import { Component, signal } from '@angular/core';
 import { ItemComponent } from './item/item.component';
+import { LoadingSkeletonComponent } from './loading-skeleton/loading-skeleton.component';
 
 @Component({
   selector: 'app-lazy-loading',
@@ -10,12 +10,6 @@ import { ItemComponent } from './item/item.component';
   templateUrl: './lazy-loading.component.html',
   styleUrl: './lazy-loading.component.scss',
 })
-export class LazyLoadingComponent implements AfterViewInit {
+export class LazyLoadingComponent {
   data = signal([1, 2, 3, 4, 5, 6, 7, 8]);
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.data.update((data) => [...data, 9, 10, 11, 12, 13, 14, 15]);
-    }, 2000);
-  }
 }
